67c0af3c7f1859882b3ee549a644e6e365a46ae7
dennismdejong <dennis@caberdo.nl>
2026-03-19 14:16:28 +0100

    Migrate testing framework imports from sdk/v2 to plugin-testing

--- azuredevops/internal/service/audit/data_audit_streams.go
+++ azuredevops/internal/service/audit/data_audit_streams.go
@@ -5,8 +5,8 @@ import (
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 
 	"github.com/microsoft/azure-devops-go-api/azuredevops/v7/audit"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/client"
--- azuredevops/internal/service/workitemtracking/utils/classification_test.go
+++ azuredevops/internal/service/workitemtracking/utils/classification_test.go
@@ -8,8 +8,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/v7/workitemtracking"
 	"github.com/microsoft/terraform-provider-azuredevops/azdosdkmocks"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/client"
