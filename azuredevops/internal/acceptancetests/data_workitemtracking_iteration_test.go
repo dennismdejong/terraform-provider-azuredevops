341888d7c889a34b695512a3c5abf4a9cc4bbe05
dennismdejong <dennis@caberdo.nl>
2026-03-19 13:28:23 +0100

    Refactor workitemtracking iteration and area based on PR review
    This commit addresses feedback from PR #1452:
    - Removes accidentally committed binary and unrelated docs
    - Renames resources to azuredevops_workitemtracking_iteration and azuredevops_workitemtracking_area
    - Decouples logic in classification.go and moves Create/Update/Delete directly into resources
    - Uses workItem.Id as the tf resource id instead of path
    - Adds identifier as a computed attribute
    - Adds format validation for names
    - Adds non-root path acceptance tests

---
+++ azuredevops/internal/acceptancetests/data_workitemtracking_iteration_test.go
@@ -0,0 +1,74 @@
+package acceptancetests
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/acceptancetests/testutils"
+)
+
+func TestAccWorkItemTrackingIterationDataSource_Read(t *testing.T) {
+	projectName := testutils.GenerateResourceName()
+	config := fmt.Sprintf(`
+%s
+
+data "azuredevops_workitemtracking_iteration" "root-iteration" {
+  project_id = azuredevops_project.project.id
+}
+
+
+`, testutils.HclProjectResource(projectName))
+
+	tfNode := "data.azuredevops_workitemtracking_iteration.root-iteration"
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testutils.PreCheck(t, nil) },
+		Providers: testutils.GetProviders(),
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(tfNode, "id"),
+					resource.TestCheckResourceAttrSet(tfNode, "project_id"),
+					resource.TestCheckResourceAttrSet(tfNode, "path"),
+					resource.TestCheckResourceAttrSet(tfNode, "name"),
+					resource.TestCheckResourceAttr(tfNode, "has_children", "true"),
+					resource.TestCheckResourceAttr(tfNode, "children.#", "3"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccWorkItemTrackingIterationDataSource_ReadNoChildren(t *testing.T) {
+	projectName := testutils.GenerateResourceName()
+	config := fmt.Sprintf(`
+%s
+
+data "azuredevops_workitemtracking_iteration" "root-iteration" {
+  project_id     = azuredevops_project.project.id
+  fetch_children = false
+}
+
+
+`, testutils.HclProjectResource(projectName))
+
+	tfNode := "data.azuredevops_workitemtracking_iteration.root-iteration"
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testutils.PreCheck(t, nil) },
+		Providers: testutils.GetProviders(),
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(tfNode, "id"),
+					resource.TestCheckResourceAttrSet(tfNode, "project_id"),
+					resource.TestCheckResourceAttrSet(tfNode, "path"),
+					resource.TestCheckResourceAttrSet(tfNode, "name"),
+					resource.TestCheckResourceAttr(tfNode, "has_children", "true"),
+					resource.TestCheckResourceAttr(tfNode, "children.#", "0"),
+				),
+			},
+		},
+	})
+}
